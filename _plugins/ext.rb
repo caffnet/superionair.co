cd067f25627600106aa10d4a09bb8e3f469ec5a7
John Stauffacher <john.stauffacher@gmail.com>
2015-06-12 14:01:22 -0400

    update ext

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,11 @@
 require "jekyll-assets"
+require "jekyll-assets/neat"
+require "jekyll-assets/compass"
+
+# bootstrap requires minimum precision of 10, see https://github.com/twbs/bootstrap-sass/issues/409
+::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+require "jekyll-assets/bootstrap"
+require "jekyll-assets/bourbon"
+require "jekyll-assets/font-awesome"
+
+
